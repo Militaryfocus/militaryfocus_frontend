52f9cdf073250a2becac3e81ec9effe5514dc61c
abdullox0900 <abdullox0900@gmail.com>
2025-01-10 20:18:21 +0500

    fix: mobile zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Wide from '@/components/Containers/Wide'
-import DisableZoom from '@/components/DisableZoom/DisableZoom'
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
 import { Open_Sans, Russo_One } from "next/font/google"
@@ -25,13 +24,11 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </head>
       <body
         className={`${russoOne.variable} ${openSans.variable} antialiased`}
         style={{ touchAction: 'none' }}
       >
-        <DisableZoom />
         <Wide>
           <Header />
           {children}
